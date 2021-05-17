bbed75141712750b194e75a5db6aa2cf44cb3a12
jasonking3 <jk1ng@yahoo.com>
2021-05-17 16:21:23 -0700

    Set NSO and Netbox image definitions in inventory

--- play.sh
+++ play.sh
@@ -4,30 +4,18 @@ OPTIONS=""
 if [[ ! -z "$ANSIBLE_VAULT_PASSWORD_FILE" ]]; then
    OPTIONS="--env ANSIBLE_VAULT_PASSWORD_FILE=/tmp/vault.pw -v $ANSIBLE_VAULT_PASSWORD_FILE:/tmp/vault.pw"
 fi
-if [[ ! -z "$CML_HOST" ]]; then
-   OPTIONS="$OPTIONS --env CML_HOST=$CML_HOST"
-fi
-if [[ ! -z "$CML_USERNAME" ]]; then
-   OPTIONS="$OPTIONS --env CML_USERNAME=$CML_USERNAME"
-fi
-if [[ ! -z "$CML_PASSWORD" ]]; then
-   OPTIONS="$OPTIONS --env CML_PASSWORD=$CML_PASSWORD"
-fi
-if [[ ! -z "$CML_LAB" ]]; then
-   OPTIONS="$OPTIONS --env CML_LAB=$CML_LAB"
-fi
-if [[ ! -z "$ANSIBLE_INVENTORY" ]]; then
-   OPTIONS="$OPTIONS --env ANSIBLE_INVENTORY=$ANSIBLE_INVENTORY"
-fi
 
 OPTION_LIST=( \
    "CML_HOST" \
    "CML_USERNAME" \
    "CML_PASSWORD" \
    "CML_LAB" \
+   "ANSIBLE_INVENTORY" \
    "CSR1000V_VERSION" \
    "UBUNTU_VERSION" \
    "IOSVL2_VERSION" \
+   "NSO_IMAGE" \
+   "NETBOX_IMAGE" \
    )
 
 for OPTION in ${OPTION_LIST[*]}; do
